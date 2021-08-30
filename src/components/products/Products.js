6345c2d772d3b6f6b58fcf0a69120728be6407da
Erin Truman <erintruman@Erins-MacBook-Pro.local>
2021-08-30 14:18:52 -0500

    added the sort method to product types

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -5,7 +5,7 @@ export const ProductList = () => {
 
     useEffect(
         () => {
-            fetch("http://localhost:8088/products?_expand=productType")
+            fetch("http://localhost:8088/products?_expand=productType&_sort=productTypeId&_order=asc")
                 .then(res => res.json())
                 .then((productData) => {
                     setProduct(productData)
@@ -22,7 +22,7 @@ export const ProductList = () => {
             {
                 products.map(
                     (product) => {
-                        return <p key={`product--${product.id}`}>{product.name} is a type of {product.productType.name}. it costs ${product.price.toFixed(2)}.</p> // every element needs a unique key. serves same purpose as an id attributeused for the rendering of the dom so it knows which element is which
+                        return <p key={`product--${product.id}`}>{product.name} is a type of {product.productType.name} Its product type id is {product.productTypeId}. It costs ${product.price.toFixed(2)}.</p> // every element needs a unique key. serves same purpose as an id attributeused for the rendering of the dom so it knows which element is which
                     }
                 )
             }
